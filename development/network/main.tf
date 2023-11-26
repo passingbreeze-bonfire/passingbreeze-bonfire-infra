05620a930fc6482f75ceefa45d476f629ca077de
passingbreeze <jungmin1237@pusan.ac.kr>
2023-11-26 19:20:12 +0900

    feat: add private subnet and fix output

--- development/network/main.tf
+++ development/network/main.tf
@@ -27,12 +27,12 @@ module "dev_vpc" {
   enable_dns_support   = true
   enable_ipv6          = true
 
-  name                        = var.dev_vpc_name
-  azs                         = tolist([for az in ["a", "c"] : "${var.dev_region}${az}"])
-  public_subnet_ipv6_native   = true
-  public_subnet_ipv6_prefixes = [0, 1]
-  #  private_subnet_ipv6_native   = true
-  #  private_subnet_ipv6_prefixes = [2, 3]
+  name                         = var.dev_vpc_name
+  azs                          = tolist([for az in ["a", "c"] : "${var.dev_region}${az}"])
+  public_subnet_ipv6_native    = true
+  public_subnet_ipv6_prefixes  = [0, 1]
+  private_subnet_ipv6_native   = true
+  private_subnet_ipv6_prefixes = [2, 3]
 
   create_egress_only_igw = true
   enable_nat_gateway     = false
--- development/network/outputs.tf
+++ development/network/outputs.tf
@@ -3,7 +3,7 @@ output "dev_vpc_id" {
 }
 
 output "dev_vpc_cidr_block" {
-  value = module.dev_vpc.vpc_cidr_block
+  value = module.dev_vpc.vpc_ipv6_cidr_block
 }
 
 output "dev_vpc_public_subnets" {
@@ -12,8 +12,4 @@ output "dev_vpc_public_subnets" {
 
 output "dev_vpc_private_subnets" {
   value = module.dev_vpc.private_subnets
-}
-
-output "dev_vpc_database_subnets" {
-  value = module.dev_vpc.database_subnets_cidr_blocks
 }

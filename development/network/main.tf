ffb0d9b710700c05585e14cb098c9a5d7091d7fa
passingbreeze <jungmin1237@pusan.ac.kr>
2023-11-26 19:07:56 +0900

    fix: ipv6 subnet prefix

--- development/network/main.tf
+++ development/network/main.tf
@@ -30,9 +30,9 @@ module "dev_vpc" {
   name                         = var.dev_vpc_name
   azs                          = tolist([for az in ["a", "c"] : "${var.dev_region}${az}"])
   public_subnet_ipv6_native    = true
-  public_subnet_ipv6_prefixes  = [56, 56]
+  public_subnet_ipv6_prefixes  = [0, 1]
   private_subnet_ipv6_native   = true
-  private_subnet_ipv6_prefixes = [56, 56]
+  private_subnet_ipv6_prefixes = [2, 3]
 
   create_egress_only_igw = true
   enable_nat_gateway     = false

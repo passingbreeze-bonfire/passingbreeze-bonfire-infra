ffb5c282ce84324a8c93a9a9f2e9217e2f565338
passingbreeze <jungmin1237@pusan.ac.kr>
2023-11-26 16:47:55 +0900

    feat: add ipam ipv6

--- development/network/vpc/main.tf
+++ development/network/vpc/main.tf
@@ -1,3 +1,23 @@
+resource "aws_vpc_ipam" "this" {
+  operating_regions {
+    region_name = var.dev_region
+  }
+
+  tags = var.dev_tags
+}
+
+resource "aws_vpc_ipam_pool" "ipv6" {
+  description                       = "IPv6 pool"
+  address_family                    = "ipv6"
+  ipam_scope_id                     = aws_vpc_ipam.this.public_default_scope_id
+  locale                            = var.dev_region
+  allocation_default_netmask_length = 56
+  publicly_advertisable             = false
+  aws_service                       = "ec2"
+
+  tags = var.dev_tags
+}
+
 module "dev_vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = ">= 5.0.0"
@@ -6,8 +26,9 @@ module "dev_vpc" {
   enable_dns_support   = true
   enable_ipv6          = true
 
-  name      = var.dev_vpc_name
-  ipv6_cidr = var.dev_vpc_cidr
+  name              = var.dev_vpc_name
+  ipv6_cidr         = var.dev_vpc_cidr
+  ipv6_ipam_pool_id = aws_vpc_ipam_pool.ipv6.id
 
   azs                                             = tolist([for az in ["a", "c"] : "${var.dev_region}${az}"])
   public_subnet_ipv6_native                       = true

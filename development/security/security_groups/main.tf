d0db139cc39a5b1a2ed7bcaa5f2097540bd607e6
passingbreeze <jungmin1237@pusan.ac.kr>
2023-12-04 02:45:30 +0900

    feat: feat cluster sg to endpoint sg

--- development/security/security_groups/main.tf
+++ development/security/security_groups/main.tf
@@ -1,4 +1,15 @@
 
+data "terraform_remote_state" "services" {
+  backend = "remote"
+
+  config = {
+    organization = "passingbreeze"
+    workspaces = {
+      name = "passingbreeze-bonfire-dev-services"
+    }
+  }
+}
+
 # aws_service_endpoint_sg
 
 module "aws_service_endpoint_sg" {
@@ -21,4 +32,13 @@ module "aws_service_endpoint_sg" {
 
   egress_rules = ["all-all"]
   tags         = var.tags
+}
+
+resource "aws_security_group_rule" "cluster_sg_rule" {
+  from_port                = 0
+  protocol                 = "-1"
+  source_security_group_id = data.terraform_remote_state.services.outputs.cluster_security_group_id
+  security_group_id        = module.aws_service_endpoint_sg.security_group_id
+  to_port                  = 0
+  type                     = "ingress"
 }

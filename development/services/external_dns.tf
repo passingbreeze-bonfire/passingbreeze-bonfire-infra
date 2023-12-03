74b11c08bbc4aa2a8caca3f9d8c8723600ffb022
passingbreeze <jungmin1237@pusan.ac.kr>
2023-12-03 15:05:07 +0900

    fix: seperate addon

---
+++ development/services/external_dns.tf
@@ -0,0 +1,168 @@
+## EKS / External DNS
+
+locals {
+  oidc_url       = replace(module.eks.cluster_oidc_issuer_url, "https://", "")
+  aws_account_id = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_iam_role" "external_dns" {
+  name = "${module.eks.cluster_id}-external-dns"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "arn:aws:iam::${local.aws_account_id}:oidc-provider/${local.oidc_url}"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "${local.oidc_url}:sub": "system:serviceaccount:kube-system:external-dns"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "external_dns" {
+  name_prefix = "${module.eks.cluster_id}-external-dns"
+  role        = aws_iam_role.external_dns.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "route53:ChangeResourceRecordSets"
+        ]
+        Resource = [
+          "arn:aws:route53:::hostedzone/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "route53:ListHostedZones",
+          "route53:ListResourceRecordSets"
+        ]
+        Resource = [
+          "*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "kubernetes_service_account" "external_dns" {
+  metadata {
+    name      = "external-dns"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.external_dns.arn
+    }
+  }
+  automount_service_account_token = true
+}
+
+resource "kubernetes_cluster_role" "external_dns" {
+  metadata {
+    name = "external-dns"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["services", "pods", "nodes"]
+    verbs      = ["get", "list", "watch"]
+  }
+  rule {
+    api_groups = ["extensions", "networking.k8s.io"]
+    resources  = ["ingresses"]
+    verbs      = ["get", "list", "watch"]
+  }
+  rule {
+    api_groups = ["networking.istio.io"]
+    resources  = ["gateways"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "external_dns" {
+  metadata {
+    name = "external-dns"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.external_dns.metadata.0.name
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.external_dns.metadata.0.name
+    namespace = kubernetes_service_account.external_dns.metadata.0.namespace
+  }
+}
+
+resource "helm_release" "external_dns" {
+  namespace        = "external-dns"
+  create_namespace = true
+
+  name       = "external-dns"
+  repository = "https://kubernetes-sigs.github.io/external-dns/"
+  chart      = "external-dns/external-dns"
+  version    = "v1.13.1"
+
+  set {
+    name  = "rbac.create"
+    value = false
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = false
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = kubernetes_service_account.external_dns.metadata.0.name
+  }
+
+  set {
+    name  = "rbac.pspEnabled"
+    value = false
+  }
+
+  set {
+    name  = "name"
+    value = "${module.eks.cluster_name}-external-dns"
+  }
+
+  set {
+    name  = "provider"
+    value = "aws"
+  }
+
+  set {
+    name  = "policy"
+    value = "sync"
+  }
+
+  set {
+    name  = "logLevel"
+    value = "warn"
+  }
+
+  set {
+    name  = "sources"
+    value = "{ingress,service}"
+  }
+
+  set {
+    name  = "aws.region"
+    value = data.aws_region.current.name
+  }
+}
